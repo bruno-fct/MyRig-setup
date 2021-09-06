8221162c89b2dc7874c9a01ba89c7e0e02ad94ea
bruno-fct <90189698+bruno-fct@users.noreply.github.com>
2021-09-06 14:16:27 +0100

    Update choco-install.ps1

--- choco-install.ps1
+++ choco-install.ps1
@@ -1,4 +1,4 @@
-# Install script for Chocolatey&Apps
+# Install script for Chocolatey & Apps
 # Created 06.09.21
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -27,20 +27,21 @@ choco install brave
 choco install firefox
 
 # Optional
-#choco install windowsazurepowershell
+choco install windowsazurepowershell
+choco install microsoft-teams
 
 choco feature disable -n allowGlobalConfirmation
 
 ECHO Installing Windows Store Apps
-Add-AppxPackage 19282JackieLiu.Notepads-Beta
-Add-AppxPackage Microsoft.WindowsTerminal
+Add-AppxPackage "19282JackieLiu.Notepads-Beta"
+Add-AppxPackage "Microsoft.WindowsTerminal"
 
 ECHO Removing Windows Store Apps
-Remove-AppxPackage Microsoft.ZuneMusic
-Remove-AppxPackage Microsoft.MicrosoftSolitaireCollection
-Remove-AppxPackage Microsoft.Office.OneNote
-Remove-AppxPackage Microsoft.MicrosoftOfficeHub
-Remove-AppxPackage Microsoft.WindowsFeedbackHub
+Remove-AppxPackage "Microsoft.ZuneMusic"
+Remove-AppxPackage "Microsoft.MicrosoftSolitaireCollection"
+Remove-AppxPackage "Microsoft.Office.OneNote"
+Remove-AppxPackage "Microsoft.MicrosoftOfficeHub"
+Remove-AppxPackage "Microsoft.WindowsFeedbackHub"
 
 ECHO Installing WSL Ubuntu v20
 wsl --install -d Ubuntu-20.04
